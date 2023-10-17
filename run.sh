42757bfd29d061715ae86762a790fdf7f6b0dad7
dedellzyw <31383717+dedellzyw@users.noreply.github.com>
2023-10-17 16:26:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,5 @@ sudo apt-get install zsh
 sh install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+cp .zshrc ~/
+source ~/.zshrc
